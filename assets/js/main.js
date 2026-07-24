03841116dd7a03a9deb23d9f4a4c67d4abb395e9
James Simmons <simmons.ben.david@gmail.com>
2026-07-24 18:01:19 +0900

    Add Google Analytics tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
 "use strict";
 
+const GOOGLE_ANALYTICS_ID = "G-KR8C4FRGRB";
+
+window.dataLayer = window.dataLayer || [];
+window.gtag = window.gtag || function gtag(){
+  window.dataLayer.push(arguments);
+};
+
+window.gtag("js", new Date());
+window.gtag("config", GOOGLE_ANALYTICS_ID);
+
+const analyticsScript = document.createElement("script");
+analyticsScript.async = true;
+analyticsScript.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`;
+document.head.appendChild(analyticsScript);
+
 import "./navigation.js";
 import "./page-transitions.js";
 import "./reading-progress.js";
