a871234a869a72d979d48a86de7fb0df1145b85f
James Simmons <simmons.ben.david@gmail.com>
2026-07-29 15:07:59 +0900

    Load corrected Genki2 presence module in lessons

--- assets/js/lesson.js
+++ assets/js/lesson.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import "./genki2/presence.js";
+import "./genki2/presence.js?v=2";
 import { createGenki2 } from "./genki2/core.js?v=4";
 
 const unit = document.querySelector("[data-genki]");
